5a56e59c9f0c4ccee931c33755fecd5375b873d1
Luqiii <althrillluxury@gmail.com>
2024-12-12 16:02:18 +0700

    api login dan register sudah konek

--- app/src/main/java/com/example/karsanusa/data/remote/response/LoginResponse.kt
+++ app/src/main/java/com/example/karsanusa/data/remote/response/LoginResponse.kt
@@ -3,12 +3,7 @@ package com.example.karsanusa.data.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class LoginResponse(
+
 	@field:SerializedName("token")
-	val token: String,
-
-	@field:SerializedName("email")
-	val email: String,
-
-	@field:SerializedName("password")
-	val password: String,
+	val token: String
 )
--- app/src/main/java/com/example/karsanusa/data/remote/response/RegisterResponse.kt
+++ app/src/main/java/com/example/karsanusa/data/remote/response/RegisterResponse.kt
@@ -4,12 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class RegisterResponse(
 
-	@field:SerializedName("password")
-	val password: String,
-
-	@field:SerializedName("fullName")
-	val fullName: String,
-
-	@field:SerializedName("email")
-	val email: String
+	@field:SerializedName("message")
+	val message: String
 )
